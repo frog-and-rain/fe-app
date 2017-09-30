763701cfcefdd07ce6304c9bf1467426af0ba74b
linh-vu <linh.vu@quoine.com>
2017-10-01 01:07:57 +0700

    update styles

--- src/App/Home/Header/Menu/Item/ItemContent/Content/View.jsx
+++ src/App/Home/Header/Menu/Item/ItemContent/Content/View.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ContentList from '../ContentList';
 import './styles.css';
 
+import leftLogo from '../../../../image/left-logo.png';
+
 const options = [
 	{text: 'item 1', link: '#'},
 	{text: 'item 2', link: '#'},
@@ -30,7 +32,13 @@ const HeaderBottomContent = () => {
 				<ContentList options={options} header={header} />
 			</div>
 			<div className="content-full-width-column">
-				<ContentList options={options} header={header} />
+				<img src={leftLogo} alt="left-logo" width={210} height={42} />
+				<p>
+					The Yoast Analytics plugin lets you easily <br />
+					connect your website to Google Analytics and <br />
+					keep track of all your site traffic and key metrics <br />
+					in real-time.
+				</p>
 			</div>
 		</div>
 	);
--- src/App/Home/Header/View.jsx
+++ src/App/Home/Header/View.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Menu from './Menu';
 import './styles.css';
+import logo from './image/logo.png';
 
 const HeaderView = () => {
 	return (
 		<div className="header">
 			<div className="header-content">
-				<div className="header-content-logo">Logo</div>
+				<div className="header-content-logo">
+					<img src={logo} width={72} height={72} alt="logo" />
+				</div>
 				<Menu />
 			</div>
 		</div>
