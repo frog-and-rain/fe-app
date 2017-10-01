5c99ef9c116755113d9087d878d3b2b0d3f516cc
linh-vu <linh.vu@quoine.com>
2017-10-01 18:39:36 +0700

    use position fixed for menu

--- src/App/Home/Header/index.jsx
+++ src/App/Home/Header/index.jsx
@@ -3,9 +3,31 @@ import React from 'react';
 import View from './View';
 
 class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.node = null;
+		
+		this.setNode = this.setNode.bind(this);
+	}
+	
+	setNode(node) {
+		this.node = node;
+	}
+	
+	componentDidMount() {
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 42) {
+				this.node.style.position = 'fixed';
+				this.node.style.top = '0';
+			} else {
+				this.node.style.position = 'inherit';
+			}
+		})
+	}
+	
 	render() {
 		return (
-			<View />
+			<View setNode={this.setNode} />
 		);
 	}
 }
